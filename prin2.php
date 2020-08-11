90f1e442bee57014b3ee8c188936459dde502cad
carlosdiazz <c.diazadriann@gmail.com>
2020-08-11 19:52:24 -0400

    Agregando reporte

--- prin2.php
+++ prin2.php
@@ -23,7 +23,7 @@
       <!-- navbar starts -->
     <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
     <div class="container">
-      <a class="navbar-brand" href="# "> <h3 class="mb-0"><i class="fas fa-fire-alt"></i></h3></a>
+      <a class="navbar-brand" href="#"> <h3 class="mb-0"><i class="fas fa-fire-alt"></i></h3></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
