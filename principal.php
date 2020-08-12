944902cdecb801d51be53593f930a20bdaedd91b
carlosdiazz <c.diazadriann@gmail.com>
2020-08-11 23:16:48 -0400

    Agregando ventana admin

--- principal.php
+++ principal.php
@@ -1,8 +1,17 @@
 <?php session_start();
 
     if(isset($_SESSION['usuario'])){
+        
+        if($_SESSION['usuario'] == "admin"){
+            header ('location: admin.php');
+        }
+        
         require 'prin.php';
-    }else{
+    }
+    
+    else
+    
+    {
         header ('location: login.php');
     }
         
